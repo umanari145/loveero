4244c5731f8c39fe7e8091e88436bdba10ae676a
umanari145 <umanari145@gmail.com>
2025-05-06 15:49:14 +0900

    appに統合するためserviceを削除

--- service/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { createUser } from '@/controllers/userController';
-import express from 'express';
-
-const router = express.Router();
-
-router.post('/users', createUser);
-
-export default router;
